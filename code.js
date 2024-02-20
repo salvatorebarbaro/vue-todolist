66bdeba3bf69c319fa58b6e23f105eb7f2acacde
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-02-20 23:02:29 +0100

    barrato elementi con classe true

--- code.js
+++ code.js
@@ -6,9 +6,23 @@ createApp({
     data() {
         return {
 
+            todos: [
+                { text: "Pulire il terrazzo", done: false },
+                { text: "Annaffiare le piante", done: true },
+                { text: "Preparare l'ombrello", done: false },
+                { text: "Controllare la previsione del tempo", done: true }
+            ],
+
         }
     },
     methods: {
+        removal()
+        {
+            if(this.current_item.done == true)
+            {
+                todos.slice(index)
+            }
+        },
         
     },
 }).mount('#app');
